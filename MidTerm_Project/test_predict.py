c81d3b700876f52417a4ae1a5bc8cf906fa608cb
Arun-Pat <112260566+Arun-Pat@users.noreply.github.com>
2023-11-05 19:41:18 +0530

    Test the webservice using test data

---
+++ MidTerm_Project/test_predict.py
@@ -0,0 +1,49 @@
+import requests
+import json
+
+participant1 = {
+    "a1_score": 1, 
+    "a2_score": 1, 
+    "a3_score": 1, 
+    "a4_score": 1, 
+    "a5_score": 0, 
+    "a6_score": 1, 
+    "a7_score": 1, 
+    "a8_score": 1, 
+    "a9_score": 1, 
+    "a10_score": 0, 
+    "age" : 17,
+    "gender": "m" , 
+    "ethnicity" : "asian", 
+    "jaundice" : "yes", 
+    "autism": "yes",  
+    "country_of_res" : "bahamas", 
+    "relation" : "health_care_professional"
+}
+
+# This is sample of NON Autistic patient
+participant2 = {
+    "a1_score": 1, 
+    "a2_score": 1, 
+    "a3_score": 0, 
+    "a4_score": 1, 
+    "a5_score": 0, 
+    "a6_score": 0, 
+    "a7_score": 1, 
+    "a8_score": 1, 
+    "a9_score": 0, 
+    "a10_score": 1, 
+    "age" : 35,
+    "gender": "f" , 
+    "ethnicity" : "white-European", 
+    "jaundice" : "no", 
+    "autism": "yes",  
+    "country_of_res" : "united_states", 
+    "relation" : "self"
+}
+
+url = "http://localhost:9696/predict"
+response = requests.post(url, json=participant2)
+result = response.json()
+
+print(json.dumps(result, indent=2))
